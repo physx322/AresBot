bd5ed46b4b09ad313b590796e33fb642db6e78da
PhysX <angledroit05@proton.me>
2026-08-04 22:28:24 +0200

    Enhance ticket dashboard message with interactive buttons and embed

--- src/main/java/org/clicdroit/listeners/TicketListener.java
+++ src/main/java/org/clicdroit/listeners/TicketListener.java
@@ -4,11 +4,15 @@ import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.channel.TextChannelCreateEvent;
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.object.PermissionOverwrite;
+import discord4j.core.object.component.*;
 import discord4j.core.object.entity.Guild;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
 import discord4j.rest.util.Permission;
 import discord4j.rest.util.PermissionSet;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
 import java.util.Set;
 
 public class TicketListener {
@@ -40,8 +44,23 @@ public class TicketListener {
     }
 
     public static Mono<Void> sendTicketDashboard(TextChannelCreateEvent event) {
+        List<LayoutComponent> components = List.of(
+                ActionRow.of(
+                        Button.danger("close_ticket", "Fermer le ticket"),
+                        Button.secondary("claim_ticket", "Prendre en charge")
+                )
+        );
+
+
         if (event.getChannel().getName().startsWith("ticket-")) {
-            return event.getChannel().createMessage("bienvenue dans votre ticket").then();
+            return event.getChannel().createMessage(MessageCreateSpec.builder()
+                            .addAllComponents(components)
+                            .addEmbed(EmbedCreateSpec.builder()
+                                    .title("Bienvenue sur votre ticket")
+                                    .description("Un membre du staff va prendre votre ticket en charge")
+                                    .build())
+                            .build())
+                    .then();
         }
         return Mono.empty();
     }
